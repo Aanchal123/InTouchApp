8074e326a71ad98b4cff1cd985c5048375bc1520
aanchal_a <aanchaldagarwal@gamil.com>
2019-01-24 15:07:23 +0530

    inTouchApp

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/IN-TOUCH-APP'));
+app.use(express.static(__dirname + '/dist/inTouchApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/IN-TOUCH-APP/index.html'));
+res.sendFile(path.join(__dirname+'/dist/inTouchApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
