cb0b92c093a65224c826e959bc608d06c31b3edd
aanchal_a <aanchaldagarwal@gamil.com>
2019-01-24 15:01:01 +0530

    SERVER

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/xyz'));
+app.use(express.static(__dirname + '/IN-TOUCH-APP/xyz'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/xyz/index.html'));
+res.sendFile(path.join(__dirname+'/dist/IN-TOUCH-APP/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
